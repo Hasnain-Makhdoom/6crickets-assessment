a2cd73be0a158505def524456868a5b9741843fe
Hasnain-Makhdoom <86113425+Hasnain-Makhdoom@users.noreply.github.com>
2025-03-11 07:53:19 -0400

    Added deadline component to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DeadlineCountdownComponent } from "./components/deadline-countdown/deadline-countdown.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DeadlineCountdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
